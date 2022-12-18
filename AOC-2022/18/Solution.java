beff10ed3b34b4fd3466ec72da3240bf4825c507
Duo Zheng <duoz@uchicago.edu>
2022-12-18 00:51:03 -0600

    AOC-2022 18

---
+++ AOC-2022/18/Solution.java
@@ -0,0 +1,391 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class Solution {
+    //idea: Part I: just multiplies 6 to number of blocks, then subtract touching surfaces with a trick
+    //Part II: do bfs starting from (0, 0, 0) (outside the blocks range)
+    //  and limit the search range to a bit wider than [0..30] for each direction, also not allowing bfs queue
+    //  to contain coordinates of blocks.
+    //  (30 - 5 (5 is for enough lee-way) is around the max coordinate for the input), the reason for us to do bfs is that we notice each outer surface
+    //  will be touched by the location in our bfs queue exactly once (i.e. when some location (i, j, k) wants to expand to its neighbor (on the cardinal direction)
+    //  (ti, tj, tk) and gets blocked by a block, we count the surface between (i, j, k) and (ti, tj, tk) once, 
+    //  and since we never allowed our bfs queue to include (ti, tj, tk) as that it the coordinate of a block, we would never count this surface again
+    //  since the only way we could do that is by having (ti, tj, tk) in queue and trying to go to (i, j, k)
+    //
+    //This bfs process of starting from outside ensures we never enter the interior or surface of the blocks, meaning we only count each surface area of blocks once.
+    //  Leading us to the desired answer.
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args){
+        int ct = 0;
+        HashSet<Long> blocks = new HashSet<>();
+        long[] max = new long[3];
+        for(String S : read_all_String()){
+            long[] arr = stol(split(S, ","));
+            //compress coordinates and add to "blocks"
+            blocks.add(arr[0] * MAX * MAX + arr[1] * MAX + arr[2]);
+            for(int i = 0; i < 3; ++i){
+                max[i] = Math.max(max[i], arr[i]);
+            }
+            ct += 6;
+        }
+        //if block ele has 2 coordinate the same as e2 and the different coordinate only differs by 1, they are adjacent
+        //--ct, later when we iterates e2 in the outer loop and ele in the inner, we reduce ct by 1 again (so that
+        //  accomplishes our goal to reduce ct by 2 for each touching surface)
+        for(long ele : blocks){
+            for(long e2 : blocks){
+                long diff = Math.abs(ele - e2);
+                if(diff == MAX * MAX || diff == MAX || diff == 1){
+                    --ct;
+                }
+            }
+        }
+
+        System.out.println("Task 1: " + ct);
+
+        int ct2 = 0;
+        Queue<Triple<Long, Long, Long>> q = new LinkedList<>();
+        HashSet<Long> visited = new HashSet<>();
+        visited.addAll(blocks);
+        q.add(new Triple<>(0L, 0L, 0L));
+        visited.add(0L);
+        int[][] dir = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+
+        //bfs search
+        loop: while(!q.isEmpty()){
+            int sz = q.size();
+            for(int s = 0; s < sz; ++s){
+                Triple<Long, Long, Long> t = q.poll();
+                long x = t.a, y = t.b, z = t.c;
+                long coord = x * MAX * MAX + y * MAX + z;
+                for(int i = 0; i < dir.length; ++i){
+                    long tx = x + dir[i][0], ty = y + dir[i][1], tz = z + dir[i][2];
+                    if(tx < -1 || ty < -1 || tz < -1 || x > 30 || y > 30 || z > 30){
+                        continue;
+                    }
+                    long ele = tx * MAX * MAX + ty * MAX + tz;
+                    //going from open location to a block -> touched a surface area, add 1 to ct2
+                    if(blocks.contains(ele)){
+                        ++ct2;
+                    }
+                    if(!visited.contains(ele)){
+                        visited.add(ele);
+                        q.add(new Triple<>(tx, ty, tz));
+                    }
+                }
+            }
+        }
+        System.out.println("Task 2: " + ct2);
+    }
+
+    //template
+    public static Scanner sc;
+    //(x + dir_card[i], y + dir_card[i + 1]) are 4 cardinal directions (for i in [0..3])
+    public static int[] dir_card = {1, 0, -1, 0, 1};
+    //same thing, 8 directions, for i in [0..7]
+    public static int[] dir_all = {1, 0, -1, 0, 1, -1, -1, 1, 1};
+
+    public static long MAX = (long)(1e6 + 0.1), MOD = 1000000007L,  _timer_ind = 0L;
+    public static long[] _timer = new long[2];
+    public static void time(){
+        _timer[(int)_timer_ind] = System.nanoTime();
+        _timer_ind = _timer_ind * -1 + 1;
+    }
+
+    //print timer result in the form of a formmated String
+    public static void print_time(){
+        System.out.println("Time: " + String.format("%.3f", (double)(_timer[1] - _timer[0]) / (double)(1e9)) + " sec");
+    }
+
+    //pad character c onto String S until it reaches target_len
+    //pad on left if side == 0, right otherwise
+    public static String pad(String S, char c, int target_len, int side){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < target_len - S.length(); ++i){
+            sb.append(c);
+        }
+        return side == 0 ? (sb.toString() + S) : (S + sb.toString());
+    }
+
+    public static int[][] rotate_90(int[][] arr){
+        int row = arr.length, col = arr[0].length;
+        int[][] res = new int[col][row];
+        for(int i = 0; i < row; ++i){
+            for(int j = 0; j < col; ++j){
+                res[j][row - 1 - i] = arr[i][j];
+            }
+        }
+        return res;
+    }
+    public static int[][] transpose(int[][] arr){
+        int row = arr.length, col = arr[0].length;
+        int[][] res = new int[col][row];
+        for(int i = 0; i < row; ++i){
+            for(int j = 0; j < col; ++j){
+                res[j][i] = arr[i][j];
+            }
+        }
+        return res;
+    }
+    //add all characters of a String into a HashSet
+    public static HashSet<Character> setify(String S){
+        HashSet<Character> set = new HashSet<>();
+        char[] a = S.toCharArray();
+        for(int i = 0; i < a.length; ++i){
+            set.add(a[i]);
+        }
+        return set;
+    }
+    public static HashSet<Character> setify(char[] a){
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0; i < a.length; ++i){
+            set.add(a[i]);
+        }
+        return set;
+    }
+    @SuppressWarnings("unchecked")
+    public static <T> T[] subarray(T[] arr, int st, int end){
+        assert(end > st);
+        T[] res = (T[]) new Object[end - st];
+        for(int i = st; i < end; ++i){
+            res[i - st] = arr[i];
+        }
+        return res;
+    }
+    public static String[] subarray(String[] arr, int st, int end){
+        assert(end > st);
+        String[] res = new String[end - st];
+        for(int i = st; i < end; ++i){
+            String S = "";
+            S += arr[i];
+            res[i - st] = S;
+        }
+        return res;
+    }
+    //split with multiple delimiters and clearing empty String
+    public static String[] split(String S, String delim){
+        String[] toks = S.split(delim);
+        List<String> l = new ArrayList<>();
+        for(String cur : toks){
+            if(!cur.equals("")){
+                l.add(cur);
+            }
+        }
+        String[] res = new String[l.size()];
+        for(int i = 0; i < res.length; ++i){
+            res[i] = l.get(i);
+        }
+        return res;
+    }
+
+    //parse a String[] into an int[]
+    public static int[] stoi(String[] toks){
+        int[] arr = new int[toks.length];
+        for(int i = 0; i < toks.length; ++i){
+            arr[i] = Integer.parseInt(toks[i]);
+        }
+        return arr;
+    }
+
+    public static long[] stol(String[] toks){
+        long[] arr = new long[toks.length];
+        for(int i = 0; i < toks.length; ++i){
+            arr[i] = Long.parseLong(toks[i]);
+        }
+        return arr;
+    }
+
+    //read all input in the form of a List of String
+    public static List<String> read_input(){
+        try{
+            sc = new Scanner(new File("input.txt"));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");        
+        }
+        List<String> l = new ArrayList<>();
+        while(sc.hasNextLine()){
+            String S = read();
+            l.add(S);
+        }
+        return l;
+    }
+
+    //read all input into a 2d char array
+    public static char[][] read_all_char_arr(){
+        List<String> l = read_input();
+        char[][] arr = new char[l.size()][l.get(0).length()];
+        for(int i = 0; i < l.size(); ++i){
+            arr[i] = l.get(i).toCharArray();
+        }
+        return arr;
+    }
+    //read all input into an int array
+    public static int[] read_all_int(){
+        List<String> l = read_input();
+        int[] arr = new int[l.size()];
+        for(int i = 0; i < l.size(); ++i){
+            arr[i] = Integer.parseInt(l.get(i));
+        }
+        return arr;
+    }
+    //read all input into a 2d int array
+    public static int[][] read_all_int_arr(){
+        char[][] arr = read_all_char_arr();
+        int row = arr.length, col = arr[0].length;
+        int[][] res = new int[row][col];
+        for(int i = 0; i < row; ++i){
+            for(int j = 0; j < col; ++j){
+                res[i][j] = (int)(arr[i][j] - '0');
+            }
+        }
+        return res;
+    }
+    //read all input into a String array
+    public static String[] read_all_String(){
+        List<String> l = read_input();
+        String[] arr = new String[l.size()];
+        for(int i = 0; i < l.size(); ++i){
+            arr[i] = l.get(i);
+        }
+        return arr;
+    }
+
+    public static <T> void print(T[] aa){
+        for(int i = 0; i < aa.length; ++i)
+            System.out.print(aa[i] + "\t");
+        System.out.println();
+    }
+    public static <T> void print(T[][] aa){
+        for(int i = 0; i < aa.length; ++i){
+            for(int j = 0; j < aa.length; ++j)
+                System.out.print(aa[i][j] + "\t");
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void print(int[] aa){
+        for(int i = 0; i < aa.length; ++i)
+            System.out.print(aa[i] + "\t");
+        System.out.println();
+    }
+    public static void print(long[] aa){
+        for(int i = 0; i < aa.length; ++i)
+            System.out.print(aa[i] + "\t");
+        System.out.println();
+    }
+    public static void print(char[] aa){
+        for(int i = 0; i < aa.length; ++i)
+            System.out.print(aa[i]);
+        System.out.println();
+    }
+    public static void print(int[][] aa){
+        for(int i = 0; i < aa.length; ++i){
+            for(int j = 0; j < aa[0].length; ++j)
+                System.out.print(aa[i][j] + "\t");
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void print(int[][][] aa){
+        for(int k = 0; k < aa[0][0].length; ++k){
+            for(int i = 0; i < aa.length; ++i){
+                for(int j = 0; j < aa[0].length; ++j){
+                    System.out.print(aa[i][j][k] + "\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void print(char[][] aa){
+        for(int i = 0; i < aa.length; ++i){
+            for(int j = 0; j < aa[0].length; ++j)
+                System.out.print(aa[i][j]);
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static <T, U> void print(HashMap<T, U> map){
+        for(T key : map.keySet()){
+            System.out.println(key + " : " + map.get(key));
+        }
+        System.out.println();
+    }
+    public static <T> void print(HashSet<T> set){
+        System.out.print("{");
+        for(T key : set){
+            System.out.print(key + ", ");
+        }
+        System.out.print("}");
+        System.out.println();
+    }
+    public static <T> void print(List<T> list){
+        System.out.print("[");
+        for(T ele : list){
+            System.out.print(ele + ", ");
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+    public static class Pair<_U, _T> { 
+        _U a;
+        _T b;
+        public Pair(_U ap, _T bp){
+            a = ap;
+            b = bp;
+        }
+    }
+    public static class Triple<_U, _T, _F>{
+        _U a;
+        _T b;
+        _F c;
+        public Triple(_U ap, _T bp, _F cp){
+            a = ap;
+            b = bp;
+            c = cp;
+        }
+    }
+    public static int[] dir = {1, 0, -1, 0, 1, 1, -1, -1, 1};
+    public static class Union{
+        int[] p, size;
+        public Union(int n){
+            p = new int[n];
+            size = new int[n];
+            Arrays.fill(size, 1);
+            for(int i = 0; i < n; ++i){
+                p[i] = i;
+            }
+        }
+        public int find(int n){
+            if(p[n] != n){
+                p[n] = find(p[n]);
+            }
+            return p[n];
+        }
+        public int union(int a, int b){
+            int fa = find(a), fb = find(b);
+            if(fa == fb){
+                return -1;
+            }
+            if(size[fa] > size[fb]){
+                p[fb] = fa;
+                size[fa] += size[fb];
+                return size[fa];
+            }
+            else{
+                p[fa] = fb;
+                size[fb] += size[fa];
+                return size[fb];
+            }
+        }
+    }
+    public static String read(){
+        return sc.nextLine();
+    }
+    public static String[] read_toks(){
+        return sc.nextLine().split(" ");
+    }
+}
